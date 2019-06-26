4d75af563d299fddba9163c2416442faba253520
Katherine Adams <katieadams919@gmail.com>
2019-06-25 21:04:41 -0500

    finished stack labs

--- src/labs_examples/datastructures_algorithms/examples/stack/labs/Exercise_01.java
+++ src/labs_examples/datastructures_algorithms/examples/stack/labs/Exercise_01.java
@@ -1,8 +1,31 @@
 package labs_examples.datastructures_algorithms.examples.stack.labs;
 
+import java.util.Stack;
+
 /**
  *      Demonstrate your mastery of Java's built-in Stack class by completing the following:
  *
  *      1) instantiate a new Stack
  *      2) demonstrate the use of EVERY method in the Stack class - there are 5
  */
+
+public class Exercise_01 {
+
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        stack.push(4);
+        stack.push(5);
+
+        System.out.println(stack.peek());
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.search(1));
+        System.out.println(stack.empty());
+
+    }
+}
--- src/labs_examples/datastructures_algorithms/examples/stack/labs/Exercise_02.java
+++ src/labs_examples/datastructures_algorithms/examples/stack/labs/Exercise_02.java
@@ -1,5 +1,10 @@
 package labs_examples.datastructures_algorithms.examples.stack.labs;
 
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *      Queues - Exercise_02
  *
@@ -16,3 +21,139 @@ package labs_examples.datastructures_algorithms.examples.stack.labs;
  *      6) contain a method to print out the data of all elements in the Stack
  *
  */
+
+public class Exercise_02 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        try {
+            stack.push(1);
+            stack.push(2);
+            stack.push(3);
+            stack.print();
+            stack.pop();
+            stack.print();
+            stack.peekFirst();
+            stack.peekLast();
+            System.out.println(stack.size());
+        } catch(Exception e) {
+            System.out.println(e);
+        }
+
+    }
+
+
+}
+
+class Stack<T> {
+
+    T[] stack;
+    int position = 0;
+
+    public Stack() {
+
+        stack = (T[])new Object[10];
+
+    }
+
+    public void push(T obj) {
+
+        stack = resize();
+        stack[position] = obj;
+        position++;
+
+    }
+
+    public void pop() throws EmptyStackException {
+
+        if(position < 0) {
+
+            throw new EmptyStackException("The stack is empty.");
+
+        } else {
+
+            stack = resize();
+            position--;
+            stack[position] = null;
+        }
+
+    }
+
+    public void peekFirst() throws EmptyStackException {
+
+        if(position < 0) {
+
+            throw new EmptyStackException("The stack is empty.");
+
+        } else {
+            System.out.println(stack[0]);
+        }
+    }
+
+    public void peekLast() throws EmptyStackException {
+
+        if(position < 0) {
+
+            throw new EmptyStackException("The stack is empty");
+
+        } else {
+
+            System.out.println(stack[position - 1]);
+        }
+    }
+
+    public int size() {
+
+        return position;
+
+    }
+
+    public T[] resize() {
+
+        if(position > 10 && position > (double)(stack.length * 0.75)) {
+
+            T[] newStack = (T[]) new Object[stack.length * 2];
+
+            for(int i = 0; i < newStack.length; i++) {
+                if(stack[i] != null) {
+                    newStack[i] = stack[i];
+                }
+            }
+
+            return newStack;
+
+        } else if(position > 10 && position < (double)(stack.length * 0.25)) {
+
+            T[] newStack = (T[]) new Object[stack.length / 2];
+
+            for(int i = 0; i < newStack.length; i++) {
+                if(stack[i] != null) {
+                    newStack[i] = stack[i];
+                }
+            }
+
+            return newStack;
+        }
+
+        return stack;
+
+    }
+
+    public void print() {
+        for(int i = 0; i < position; i++) {
+            System.out.println(stack[i]);
+        }
+    }
+
+}
+
+class EmptyStackException extends Exception {
+
+    EmptyStackException(String s) {
+
+        super(s);
+
+    }
+
+}
